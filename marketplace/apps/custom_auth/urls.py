5e601a1954d6983e1b7bcd75426df695bf1934ea
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2020-09-15 14:25:07 +0200

    first commit

---
+++ marketplace/apps/custom_auth/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('', views.Dashboard, name='home'),
+    path('logout/', views.Logout, name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+]
